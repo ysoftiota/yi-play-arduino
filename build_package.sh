b925b23fb7b66cc93a7309543223e240863c6e1d
Stepan Novacek <stepan.novacek@ysoft.com>
2017-04-04 11:46:58 +0200

    driver installation package

--- build_package.sh
+++ build_package.sh
@@ -7,12 +7,22 @@ else
   read VERSION
 fi
 
+BUILDDIR="build"
 JSONFILE="package_iotaysoftcom_index.json"
 ARCHIVE="YSoft-samd-${VERSION}.tar.gz"
-IGNOREFILES="build_package* .git* ${JSONFILE}* *.gz"
+IGNOREFILES="$BUILDDIR build_package* .git* ${JSONFILE}* *.gz"
+DRIVERSARCHIVE="YSoft-drivers-${VERSION}.zip"
+
+if [ ! -e "$BUILDDIR" ];then
+    mkdir $BUILDDIR
+fi
 
 if [ ! -e ${ARCHIVE} ]; then
-    tar `for i in $IGNOREFILES; do echo -n "--exclude=./$i "; done` -czf ${ARCHIVE} .
+    tar `for i in $IGNOREFILES; do echo -n "--exclude=./$i "; done` -czf $BUILDDIR/${ARCHIVE} .
+fi
+
+if [ ! -e ${DRIVERSARCHIVE} ]; then
+    zip -r $BUILDDIR/$DRIVERSARCHIVE drivers
 fi
 
 echo "Build OK"
